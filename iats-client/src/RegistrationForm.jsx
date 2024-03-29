eeceb9fac69b2b48b8a47c1ad4949f1fdd16320b
Sandip Gurung <sandip.gurung@hotmail.com>
2024-03-29 15:34:06 +0000

    external css added for registrationform.jsx

--- iats-client/src/RegistrationForm.jsx
+++ iats-client/src/RegistrationForm.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
+import './RegistrationForm.css';
 
 const RegistrationForm = () => {
+
     return (
-        <form method="POST">
-            <div class="container">
+        <form className="registration-form" method="POST">
+            <div className="container">
                 <h2>Registration Form</h2>
                 <label for="email"><b>Email</b></label>
                 <input type="text" placeholder="Email"/>
