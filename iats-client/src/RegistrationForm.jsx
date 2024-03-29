54411c9c8acec6ccec47338d59e04a2820d30ebf
Sandip Gurung <sandip.gurung@hotmail.com>
2024-03-29 23:06:12 +0000

    added css to registrationform.jsx, need input validation i.e., password length response

--- iats-client/src/RegistrationForm.jsx
+++ iats-client/src/RegistrationForm.jsx
@@ -5,11 +5,23 @@ import './RegistrationForm.css';
 const RegistrationForm = () => {
 
     return (
-        <form className="registration-form" method="POST">
-            <div className="container">
+        <form>
+            <div className="container-register">
                 <h2>Registration Form</h2>
-                <label for="email"><b>Email</b></label>
-                <input type="text" placeholder="Email"/>
+                <p>Please, fill in this form to create an account.</p>
+                <hr/>
+                <label htmlFor="email"><b>Email</b></label>
+                <input type="text" placeholder="Enter Email" className="email" id="email" required/>
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" className="psw" id="psw" required/>
+                <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
+                <input type="password" placeholder="Repeat Password" className="psw-repeat" id="psw-repeat" required/>
+                <hr/>
+                <p>By creating an account, you agree to our <a href="#">Terms & Privacy</a>.</p>
+                <button type="submit" className="registerBtn">Register</button>
+            </div>
+            <div className="container-signin">
+                <p>Already have an account? <a href='#'>Sign In</a>.</p>
             </div>
         </form>
     );    
