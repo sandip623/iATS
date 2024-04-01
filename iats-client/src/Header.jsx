c3820ec958be718655122235977783b8170454e2
Sandip Gurung <sandip.gurung@hotmail.com>
2024-04-01 23:30:52 +0100

    header css sorted for signed out

--- iats-client/src/Header.jsx
+++ iats-client/src/Header.jsx
@@ -4,13 +4,13 @@ import './Header.css';
 
 const AccountDropdown = () => {
     return (
-        <div className="accountDropDown">
+        <>
             <Link to="/SignIn"><b>ACCOUNT</b></Link>
             <ul class="accountDropDownContent">
                 <li><Link to="/Register"><b>Registration</b></Link></li>
                 <li><Link to="/SignIn"><b>Sign In</b></Link></li>
             </ul>
-        </div>
+        </>
     )
 }
 
@@ -22,12 +22,13 @@ const Header = () => {
                 {
                     loggedIn==false ?
                     <ul>
-                        <li><Link to="/Home"><b>HOME</b></Link></li>
+                        <li className="home"><Link to="/Home"><b>HOME</b></Link></li>
                         {/* 
                         <li><Link to="/Register"><b>REGISTER</b></Link></li>
                         <li><Link to="/SignIn"><b>SIGN IN</b></Link></li>
                         */}
-                        <li><AccountDropdown/></li>
+                        <li className="accountDropDown"><AccountDropdown/></li>
+                        <li className="support"><Link to="/Support"><b>SUPPORT</b></Link></li>
                     </ul>
                     :
                     <ul>
