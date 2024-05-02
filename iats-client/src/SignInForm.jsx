532083ddcd2bcef4317b7bad3b215adbec09f570
Sandip Gurung <sandip.gurung@hotmail.com>
2024-05-02 18:31:45 +0100

    began signin page frontend

--- iats-client/src/SignInForm.jsx
+++ iats-client/src/SignInForm.jsx
@@ -4,11 +4,16 @@ const SigninForm = () => {
     return (
         <form method="POST">
             <div className="container-signin">
-                <h2><b>Log In</b></h2>
-                <p>Sign In </p>
+                <h2><b>Sign In</b></h2>
+                <p>Please, fill in this form with your account details</p>
                 <hr/>
-                <label htmlFor="email">Email</label>
-                <input type="text"/>
+                <label htmlFor="email"><b>Email</b></label>
+                <input type="text" className="email"/>
+                <label htmlFor="pwd"><b>Password</b></label>
+                <input type="password" className="pwd"/>
+                <hr/>
+                {/* we want to rename the button class for signin */}
+                <button type="submit" className="registerBtn"/>
             </div>
         </form>
     );
